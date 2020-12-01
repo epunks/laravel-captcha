0a7efffacc50cc941f37a7044d24df4c757b7f06
Nils <nils@epunks.de>
2020-12-01 17:29:49 +0100

    blade template mit id param

--- src/resources/views/vendor/bone/captcha/image.blade.php
+++ src/resources/views/vendor/bone/captcha/image.blade.php
@@ -1,6 +1,6 @@
-<img src="{{ $route }}"
-     alt="https://github.com/igoshev/laravel-captcha"
-     style="cursor:pointer;width:{{ $width }}px;height:{{ $height }}px;"
+<img id="{{ $id }}" src="{{ $captchaImage }}"
+     alt=""
+     style="cursor:pointer;width: 100%;height:{{ $height }}px; border: 1px solid #ced4da; border-radius: 0.25rem;"
      title="{{ $title }}"
-     onclick="this.setAttribute('src','{{ $route }}&_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
+     onclick="this.setAttribute('src','{{ $route }}&_='+Math.random()+'&captcha-id={{ $id }}');var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
 >
